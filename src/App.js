c3638d4ec4932ffbf284e64c38af8fa5d9fb0dbf
jenross <ross.m.jennifer@gmail.com>
2020-09-18 05:45:05 -0400

    initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <main className="layout">
+        <div className="box" />
+      </main>
+    </div>
+  );
+}
